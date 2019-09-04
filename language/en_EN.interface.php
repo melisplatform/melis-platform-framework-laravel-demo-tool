2afd5c4788e478a7a764ac74abb97cf86ce84ab5
bryan <rbbrioso28@gmail.com>
2019-09-04 15:18:16 +0800

    Views and translation modified

--- language/en_EN.interface.php
+++ language/en_EN.interface.php
@@ -1,6 +1,4 @@
 <?php
 return [
     'tr_melisplatformframeworklaraveldemotool_tool' => 'Laravel Tool Demo',
-	'tr_meliscodeexamplelaravel_title' => 'A tool embryo made in Laravel',
-	'tr_meliscodeexamplelaravel_desc' => 'This demo tool has for goal to show how you can use the framework Laravel inside Melis Platform, so you\'re not locked on ZF',
 ];
--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -1,6 +1,4 @@
 <?php
 return [
     'tr_melisplatformframeworklaraveldemotool_tool' => 'Outil Demo en Laravell',
-    'tr_meliscodeexamplelaravel_title' => 'Un embryon d\'outil fait en Laravel',
-	'tr_meliscodeexamplelaravel_desc' => 'Cet outil de demo a pour but de montrer comment utiliser le framework Laravel à l\'intérieur de Melis Platform, et ne pas être bloqué en ZF',
 ];
