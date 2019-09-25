5a5976f922f888a5dcd8494648aec5ad38bad90c
bryan <rbbrioso28@gmail.com>
2019-09-25 17:34:52 +0800

    Composer scripts added and translation updated

--- language/en_EN.interface.php
+++ language/en_EN.interface.php
@@ -3,4 +3,5 @@ return [
     'tr_melisplatformframeworklaraveldemotool_tool' => 'Laravel Tool Demo',
     'tr_melisplatformframeworklaraveldemotool_plugin_title' => 'Laravel plugin',
     'tr_melisplatformframeworklaraveldemotool_plugin_desc' => 'This plugin displays content requested from Laravel',
+    'tr_PluginSection_melisplatformframeworklaraveldemotool' => 'Laravel Tool Demo',
 ];
--- language/fr_FR.interface.php
+++ language/fr_FR.interface.php
@@ -3,4 +3,5 @@ return [
     'tr_melisplatformframeworklaraveldemotool_tool' => 'Outil Demo en Laravell',
     'tr_melisplatformframeworklaraveldemotool_plugin_title' => 'Laravel plugin',
     'tr_melisplatformframeworklaraveldemotool_plugin_desc' => 'Ce plugin affiche du contenu tiré de Symfony',
+    'tr_PluginSection_melisplatformframeworklaraveldemotool' => 'Laravel Tool Demo',
 ];
